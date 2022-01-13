73682162568d03e6e9a1c3951182a53c5dd42f5d
Phillip Truong <TruongHPhillip@gmail.com>
2022-01-13 14:02:27 -0500

    imported polaris, updated app page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Page, Card, Layout } from '@shopify/polaris';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Page title="Spacestagram" subtitle='Image-sharing from the final frontier'>
+        <Layout>
+          <Card title="Images" sectioned>
+            <p>Nasa images go here :p </p>
+          </Card>
+        </Layout>
+      </Page>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Polaris imports
+import '@shopify/polaris/build/esm/styles.css';
+import {AppProvider} from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
+
+const WrappedApp = () => {
+  return (
+    <React.StrictMode>
+      <AppProvider i18n={enTranslations}>
+        <App />
+      </AppProvider>
+    </React.StrictMode>
+  )
+}
+
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
